e2b2e447abbf5935936e4e552f44a8600d126c79
juanpabloct <juanptorres2003@gmail.com>
2023-06-27 11:11:26 -0500

    Add settigns for graphgql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
+import { join } from 'path';
+import { TiendaModule } from './tienda/tienda.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TiendaModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
