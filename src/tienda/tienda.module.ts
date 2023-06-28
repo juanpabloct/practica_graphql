e9f79397f3a3cdba255da44acb6588842c123577
juanpabloct <juanptorres2003@gmail.com>
2023-06-28 17:32:42 -0500

    remove configModule

--- src/tienda/tienda.module.ts
+++ src/tienda/tienda.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TiendaResolver } from './tienda.resolver';
 import { TiendaService } from './tienda.service';
 import { PrismaDbModule } from 'src/prisma-db/prisma-db.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaDbModule],
+  imports: [PrismaDbModule],
   providers: [TiendaResolver, TiendaService],
 })
 export class TiendaModule {}
