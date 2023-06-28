8d6bcfc85a8dd6a7d52f3d4e0fb6fa64b00e6ba8
juanpabloct <juanptorres2003@gmail.com>
2023-06-28 17:27:59 -0500

    Import PrismaDbModule

--- src/productos/productos.module.ts
+++ src/productos/productos.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductosResolver } from './productos.resolver';
 import { ProductosService } from './productos.service';
+import { PrismaDbModule } from 'src/prisma-db/prisma-db.module';
 
 @Module({
   providers: [ProductosResolver, ProductosService],
+  imports: [PrismaDbModule],
 })
 export class ProductosModule {}
