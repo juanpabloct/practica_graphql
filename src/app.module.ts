248c25998af0435b480263ddc311986d4f77eee9
juanpabloct <juanptorres2003@gmail.com>
2023-06-28 07:39:05 -0500

    Insert modules in lazy load

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-
 import { join } from 'path';
-import { TiendaModule } from './tienda/tienda.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -13,7 +12,14 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
-    TiendaModule,
+    forwardRef(() =>
+      import('./tienda/tienda.module').then((module) => module.TiendaModule),
+    ),
+    forwardRef(() =>
+      import('./productos/productos.module').then(
+        (module) => module.ProductosModule,
+      ),
+    ),
   ],
   controllers: [],
   providers: [],
