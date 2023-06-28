cf320bbc8994cbfb7f1908fd7a53d7206fbd5246
juanpabloct <juanptorres2003@gmail.com>
2023-06-28 12:38:54 -0500

    modify modules from lazy loading

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,33 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
       autoSchemaFile: join(process.cwd(), './schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
-    forwardRef(() =>
-      import('./tienda/tienda.module').then((module) => module.TiendaModule),
+    forwardRef(
+      async () =>
+        await import('./tienda/tienda.module').then(
+          (module) => module.TiendaModule,
+        ),
     ),
-    forwardRef(() =>
-      import('./productos/productos.module').then(
-        (module) => module.ProductosModule,
-      ),
+    forwardRef(
+      async () =>
+        await import('./productos/productos.module').then(
+          (module) => module.ProductosModule,
+        ),
+    ),
+    forwardRef(
+      async () =>
+        await import('./prisma-db/prisma-db.module').then(
+          (module) => module.PrismaDbModule,
+        ),
     ),
   ],
   controllers: [],
