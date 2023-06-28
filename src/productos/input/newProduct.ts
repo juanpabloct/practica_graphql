8fe8f7a6aaa757c07b5a3678a10f2c1ab6b25f76
juanpabloct <juanptorres2003@gmail.com>
2023-06-28 17:32:11 -0500

    change type

--- src/productos/input/newProduct.ts
+++ src/productos/input/newProduct.ts
@@ -1,10 +1,13 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsNumber, IsPositive, IsString } from 'class-validator';
 
 @InputType()
 export class NewProductInput {
+  @IsString()
   @Field(() => String)
   name: string;
-
+  @IsNumber()
+  @IsPositive()
   @Field(() => Int)
-  id: number;
+  precio: number;
 }
