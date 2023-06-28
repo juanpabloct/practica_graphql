45d762d2cde5c687bd7b34d9a081c87435b4b752
juanpabloct <juanptorres2003@gmail.com>
2023-06-28 07:39:40 -0500

    add validations global

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import * as compression from 'compression';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
