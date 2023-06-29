f9281639359a4d0f034dcd139ae915af89aec624
juanpabloct <juanptorres2003@gmail.com>
2023-06-29 12:27:04 -0500

    New customPipe for transform password

---
+++ src/common/encript-password/encrypt-password.pipe.ts
@@ -0,0 +1,13 @@
+import { Injectable, PipeTransform } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { EncryptPaswordService } from '../encryptPassword.service';
+
+@Injectable()
+export class EncryptPasswordPipe implements PipeTransform {
+  constructor(private readonly encrypt: EncryptPaswordService) {}
+  transform(value: User) {
+    const encryptPassword = this.encrypt.encryptPassword(value.password);
+    value.password = encryptPassword;
+    return value;
+  }
+}
