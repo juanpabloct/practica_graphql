2a5176f8b28ccbfbf7546c2eeaffe20d6dd83e03
juanpabloct <juanptorres2003@gmail.com>
2023-07-04 11:53:43 -0500

    Import modules

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { PrismaDbModule } from 'src/prisma-db/prisma-db.module';
 import { CommonModule } from 'src/common/common.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [UserResolver, UserService],
-  imports: [PrismaDbModule, CommonModule],
+  imports: [
+    PrismaDbModule,
+    forwardRef(() => CommonModule),
+    forwardRef(() => AuthModule),
+  ],
+  exports: [UserService],
 })
 export class UserModule {}
