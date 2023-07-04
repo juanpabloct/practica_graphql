9aea64ac7cdb4c5f6105b7989d4459f2d33bba2f
juanpabloct <juanptorres2003@gmail.com>
2023-07-04 11:52:48 -0500

    Add promise

--- src/common/encryptPassword.service.ts
+++ src/common/encryptPassword.service.ts
@@ -6,7 +6,7 @@ export class EncryptPaswordService {
   encryptPassword(password: string) {
     return hashSync(password, 10);
   }
-  EqualPassword(password: string, encryptPassword: string) {
+  async EqualPassword(password: string, encryptPassword: string) {
     return compare(password, encryptPassword);
   }
 }
