48826eafe7e16b565549cd299a63fc979a76c445
juanpabloct <juanptorres2003@gmail.com>
2023-07-04 11:51:21 -0500

    Login user input

---
+++ src/auth/inputs/sing-in.input.ts
@@ -0,0 +1,13 @@
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsJWT, IsNumber } from 'class-validator';
+import { CreateUserInput } from './create-user.input';
+
+@InputType()
+export class SingInInput extends CreateUserInput {
+  @Field(() => String, { description: 'return Token ' })
+  @IsJWT()
+  token: string;
+  @Field(() => Int, { description: 'return Token ' })
+  @IsNumber()
+  idUser: number;
+}
