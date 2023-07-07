907fff155e6509b4d99c3a656e7a62dbe159b3fc
juanpabloct <juanptorres2003@gmail.com>
2023-07-06 23:37:44 -0500

    Change types for args

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -4,14 +4,14 @@ import { UsePipes } from '@nestjs/common';
 import { EncryptPasswordPipe } from 'src/common/encript-password/encrypt-password.pipe';
 import { CreateUserInput } from 'src/auth/inputs/create-user.input';
 import { Login } from 'src/user/entities/user.entity';
-import { User } from 'src/@generated/prisma-nestjs-graphql/user/user.model';
+import { RolAnduser } from 'src/@generated/prisma-nestjs-graphql/rol-anduser/rol-anduser.model';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
   @UsePipes(EncryptPasswordPipe)
-  @Mutation(() => User)
+  @Mutation(() => RolAnduser)
   async singUp(@Args('SingUp') createUserInput: CreateUserInput) {
     const newUser = await this.authService.SingUp(createUserInput);
     return newUser;
