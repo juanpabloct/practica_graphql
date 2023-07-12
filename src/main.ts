794408875583ee056b95ed521df20259bc500ac1
juanpabloct <juanptorres2003@gmail.com>
2023-07-12 01:57:28 -0500

    order imports

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './prisma-db/prisma-db.service';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import compression from '@fastify/compress';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  ); 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-  await app.register(compression, { encodings: ['gzip', 'deflate'], global:true, threshold:300  });
-  await app.listen(3000, );
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
+	const prismaService = app.get(PrismaService);
+	await prismaService.enableShutdownHooks(app);
+	await app.register(compression, { encodings: ['gzip', 'deflate'], global: true, threshold: 300 });
+	await app.listen(3000);
 }
 bootstrap();
