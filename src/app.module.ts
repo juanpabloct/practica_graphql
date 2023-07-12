c2aa7edf7c4c1e874b1972cc48ac9404041fbc51
juanpabloct <juanptorres2003@gmail.com>
2023-07-11 22:33:02 -0500

    import modules for lazy loading

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,37 @@
-import { Module, forwardRef } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaDbModule } from './prisma-db/prisma-db.module';
-import { CommonModule } from './common/common.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module, forwardRef } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      playground: false,
-      autoSchemaFile: join(process.cwd(), './schema.gql'),
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-    }),
-    //TiendaModule,
-    forwardRef(async()=>(await import("./tienda/tienda.module")).TiendaModule),
-    //ProductosModule,
-    forwardRef(async()=>(await import("./productos/productos.module")).ProductosModule),
-    //UserModule,
-    forwardRef(async()=>(await import("./user/user.module")).UserModule),
-    CommonModule,
-    PrismaDbModule,
-    //Module of the autentication
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+	imports: [
+		ConfigModule.forRoot(),
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			playground: false,
+			autoSchemaFile: join(process.cwd(), './schema.gql'),
+			plugins: [ApolloServerPluginLandingPageLocalDefault()],
+		}),
+		//TiendaModule,
+		forwardRef(async () => (await import('./tienda/tienda.module')).TiendaModule),
+		//ProductosModule,
+		forwardRef(async () => (await import('./productos/productos.module')).ProductosModule),
+		//UserModule,
+		forwardRef(async () => (await import('./user/user.module')).UserModule),
+		//CommonModule,
+		forwardRef(async () => (await import('./common/common.module')).CommonModule),
+		PrismaDbModule,
+		//Module of the autentication
+		AuthModule,
+		forwardRef(
+			async () => (await import('./bodega-tienda-producto/bodega-tienda-producto.module')).BodegaTiendaProductoModule,
+		),
+		//BodegaTiendaProductoModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
