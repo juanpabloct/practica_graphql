c5e0ebe536a63fee76cc9d1acbb7c54dc7987b3a
juanpabloct <juanptorres2003@gmail.com>
2023-07-11 22:42:59 -0500

    Change type for resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,35 +1,34 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { UserService } from './user.service';
-import { UserWithoutPassword, UserObject } from './entities/user.entity';
 import { UpdateUserInput } from './dto/inputs/update-user.input';
+import { UserObject, UserWithoutPassword } from './entities/user.entity';
+import { UserService } from './user.service';
 import { UseGuards, UsePipes } from '@nestjs/common';
-import { EncryptPasswordPipe } from 'src/common/encript-password/encrypt-password.pipe';
-import { JwtAuthGuard } from 'src/auth/guards/jwtGuard';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from 'src/@generated/prisma-nestjs-graphql/user/user.model';
-
+import { JwtAuthGuard } from 'src/auth/guards/jwtGuard';
+import { EncryptPasswordPipe } from 'src/common/encript-password/encrypt-password.pipe';
 @Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
-  //@UseGuards(JwtAuthGuard)
-  @Query(() => [User], { name: 'users' })
-  async findAll() {
-    const users =await this.userService.findAll();
-    return users
-  }
+	constructor(private readonly userService: UserService) {}
+	@UseGuards(JwtAuthGuard)
+	@Query(() => [UserWithoutPassword], { name: 'users' })
+	async findAll() {
+		const users = await this.userService.findAll();
+		return users;
+	}
 
-  @Query(() => User, { name: 'user' })
-  async findOne(@Args('id', { type: () => Int }) id: number) {
-    return await this.userService.findOne(id);
-  }
+	@Query(() => User, { name: 'user' })
+	async findOne(@Args('id', { type: () => Int }) id: number) {
+		return await this.userService.findOne(id);
+	}
 
-  @UsePipes(EncryptPasswordPipe)
-  @Mutation(() => User)
-  async updateUser(@Args('updateUser') update: UpdateUserInput) {
-    return await this.userService.update(update);
-  }
+	@UsePipes(EncryptPasswordPipe)
+	@Mutation(() => User)
+	async updateUser(@Args('updateUser') update: UpdateUserInput) {
+		return await this.userService.update(update);
+	}
 
-  @Mutation(() => UserObject)
-  async removeUser(@Args('id', { type: () => Int }) id: number) {
-    return await this.userService.remove(id);
-  }
+	@Mutation(() => UserObject)
+	async removeUser(@Args('id', { type: () => Int }) id: number) {
+		return await this.userService.remove(id);
+	}
 }
