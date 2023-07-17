be1be0397c09aa4c424b5b261cef03e04e8d11fe
juanpabloct <=>
2023-07-17 18:37:31 -0500

    add service singOff

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,12 @@
-import { BadRequestException, Inject, Injectable, Logger, forwardRef } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { BadRequestException, Inject, Injectable, Logger, NotFoundException, forwardRef } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 
-import { SingInInput } from './inputs/sing-in.input';
-import { RolAnduser } from 'src/@generated/prisma-nestjs-graphql/rol-anduser/rol-anduser.model';
-import { CreateUserInput } from 'src/auth/inputs/create-user.input';
-import { EncryptPaswordService } from 'src/common/encryptPassword.service';
-import { PrismaService } from 'src/prisma-db/prisma-db.service';
-import { UserService } from 'src/user/user.service';
+import { SingInInput } from './inputs/sing-in.input'
+import { RolAnduser } from 'src/@generated/prisma-nestjs-graphql/rol-anduser/rol-anduser.model'
+import { CreateUserInput } from 'src/auth/inputs/create-user.input'
+import { EncryptPaswordService } from 'src/common/encryptPassword.service'
+import { PrismaService } from 'src/prisma-db/prisma-db.service'
+import { UserService } from 'src/user/user.service'
 @Injectable()
 export class AuthService {
 	constructor(
@@ -16,11 +16,12 @@ export class AuthService {
 		private readonly userService: UserService,
 		private readonly jwt: JwtService,
 	) {}
-	logger = new Logger();
+	entiti = this.prisma.rolAnduser
+	logger = new Logger()
 
 	async SingUp({ Rol, email, password }: CreateUserInput): Promise<RolAnduser> {
 		try {
-			const newUser = await this.prisma.rolAnduser.create({
+			const newUser = await this.entiti.create({
 				data: {
 					User: {
 						create: {
@@ -28,40 +29,30 @@ export class AuthService {
 							password,
 						},
 					},
-					RolAndPermiso: {
-						create: {
-							Rol: {
-								connectOrCreate: {
-									create: {
-										name: Rol,
-									},
-									where: {
-										name: Rol,
-									},
-								},
+					rol: {
+						connectOrCreate: {
+							create: {
+								name: Rol,
+							},
+							where: {
+								name: Rol,
 							},
 						},
 					},
 				},
 				include: {
+					rol: true,
 					User: true,
-					RolAndPermiso: {
-						include: {
-							Permiso: true,
-							Rol: true,
-							_count: true,
-						},
-					},
 				},
-			});
-			return newUser;
+			})
+			return newUser
 		} catch (error) {
-			throw new BadRequestException({ message: error.message });
+			throw new BadRequestException({ message: error.message })
 		}
 	}
 	async SingIn({ email, password }: SingInInput) {
 		try {
-			const { password: passwordEmail, ...infoUser } = await this.userService.findForEmail(email);
+			const { password: passwordEmail, ...infoUser } = await this.userService.findForEmail(email)
 			if (!infoUser.active) {
 				const { active } = await this.prisma.user.update({
 					data: {
@@ -73,25 +64,51 @@ export class AuthService {
 					select: {
 						active: true,
 					},
-				});
-				infoUser.active = active;
+				})
+				infoUser.active = active
 			}
-			const validatePassword = await this.passwordService.EqualPassword(password, passwordEmail);
+			const validatePassword = await this.passwordService.EqualPassword(password, passwordEmail)
 			if (validatePassword) {
-				const tokenGenerate = this.jwt.sign(infoUser);
-				return { password: passwordEmail, ...infoUser, token: tokenGenerate };
+				const tokenGenerate = this.jwt.sign(infoUser)
+				return { password: passwordEmail, ...infoUser, token: tokenGenerate }
 			} else {
-				throw new BadRequestException('Credentiasl are incorrects');
+				throw new BadRequestException('Credentiasl are incorrects')
 			}
 		} catch (error) {
-			this.ErrorLogin(error);
-			return error;
+			this.ErrorLogin(error)
+			return error
+		}
+	}
+	async signOff(id: number) {
+		const user = await this.userService.findOne(id)
+		if (user?.active) {
+			const updateData = this.entiti.update({
+				where: {
+					id,
+				},
+				data: {
+					User: {
+						update: {
+							active: false,
+						},
+					},
+				},
+				include: {
+					rol: true,
+					User: true,
+				},
+			})
+			return updateData
+		} else if (user) {
+			return user
+		} else {
+			throw new NotFoundException("user doesn't exist")
 		}
 	}
 	ErrorLogin(error: Error) {
-		this.logger.error(error.message);
+		this.logger.error(error.message)
 	}
 	async validateUser(id: number) {
-		return this.userService.findOne(id);
+		return this.userService.findOne(id)
 	}
 }
