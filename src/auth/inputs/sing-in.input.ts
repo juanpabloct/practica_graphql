1a63f4d2920cddc26421ae1ec3a5f7c6db453c5c
juanpabloct <=>
2023-07-17 18:39:37 -0500

    Input for singInInput

--- src/auth/inputs/sing-in.input.ts
+++ src/auth/inputs/sing-in.input.ts
@@ -1,5 +1,5 @@
-import { CreateUserInput } from './create-user.input';
-import { InputType, OmitType } from '@nestjs/graphql';
+import { InputType, OmitType } from '@nestjs/graphql'
+import { UserCreateWithoutRolAnduserInput } from 'src/@generated/prisma-nestjs-graphql/user/user-create-without-rol-anduser.input'
 
 @InputType()
-export class SingInInput extends OmitType(CreateUserInput, ['Rol', 'active']) {}
+export class SingInInput extends OmitType(UserCreateWithoutRolAnduserInput, ['active']) {}
