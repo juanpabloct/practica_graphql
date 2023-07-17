ad4c650cc3e9b1f7b9cf349b17b85ca4fe7cca5e
juanpabloct <=>
2023-07-17 18:55:41 -0500

    change  name querys and mutation to lowerCase

--- src/tienda/tienda.resolver.ts
+++ src/tienda/tienda.resolver.ts
@@ -1,22 +1,22 @@
-import { NewTiendaInput, TiendaSchema } from './inputs/tienda';
-import { TiendaService } from './tienda.service';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { NewTiendaInput, TiendaSchema } from './inputs/tienda'
+import { TiendaService } from './tienda.service'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
 @Resolver()
 export class TiendaResolver {
 	constructor(private readonly tiendaService: TiendaService) {}
 	@Query(() => [TiendaSchema], {
-		name: 'AllTiendas',
+		name: 'allTiendas',
 		description: 'Get all Tiendas',
 	})
 	async tiendas() {
-		return await this.tiendaService.getAllTiendas();
+		return await this.tiendaService.getAllTiendas()
 	}
 	@Mutation(() => TiendaSchema, {
 		name: 'newShop',
 		description: 'Add new Shop',
 	})
 	async newTienda(@Args('newTienda') tienda: NewTiendaInput) {
-		return await this.tiendaService.addShop(tienda);
+		return await this.tiendaService.addShop(tienda)
 	}
 }
