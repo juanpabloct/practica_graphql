bf0d5167ab1db8c6f683321c1ae115078f7f0d5b
juanpabloct <=>
2023-07-17 18:39:04 -0500

    Input for create User

--- src/auth/inputs/create-user.input.ts
+++ src/auth/inputs/create-user.input.ts
@@ -1,14 +1,14 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { IsOptional } from 'class-validator';
-import { UserCreateWithoutRolAnduserInput } from 'src/@generated/prisma-nestjs-graphql/user/user-create-without-rol-anduser.input';
+import { Field, InputType, OmitType } from '@nestjs/graphql'
+import { IsOptional } from 'class-validator'
+import { UserCreateWithoutRolAnduserInput } from 'src/@generated/prisma-nestjs-graphql/user/user-create-without-rol-anduser.input'
 
 @InputType()
-export class CreateUserInput extends UserCreateWithoutRolAnduserInput {
+export class CreateUserInput extends OmitType(UserCreateWithoutRolAnduserInput, ['active']) {
 	@Field(() => String, {
 		description: 'Column for Rol',
 		defaultValue: 'User',
 		nullable: true,
 	})
 	@IsOptional()
-	Rol: string;
+	Rol?: string
 }
