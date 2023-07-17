55fa757db32f3699050d53c8704b0ca2424b5399
juanpabloct <=>
2023-07-17 18:41:03 -0500

    Validate user is Active

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,26 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PayloadToken } from './interfaces/tokenDesencrypt'
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor(configService: ConfigService) {
+	constructor(configService: ConfigService, private User: UserService) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
 			secretOrKey: configService.get('JWT_SECRET'),
-		});
+		})
 	}
 
-	async validate(payload) {
-		return { userId: payload.sub, username: payload.username };
+	async validate(payload: PayloadToken) {
+		const { active } = await this.User.findOne(payload.id)
+		if (active) {
+			return { userId: payload.id, email: payload.email, rol: payload.RolAnduser[0].rol.name }
+		} else {
+			throw new UnauthorizedException('User is not Autorizd ')
+		}
 	}
 }
