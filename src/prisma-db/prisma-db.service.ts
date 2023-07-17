514e6a5e4a4b70c231ec1abc15b29c3713ce7eb8
juanpabloct <=>
2023-07-17 18:46:54 -0500

    connect db prisma

--- src/prisma-db/prisma-db.service.ts
+++ src/prisma-db/prisma-db.service.ts
@@ -1,11 +1,9 @@
-import { INestApplication, Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
-  }
+	async onModuleInit() {
+		await this.$connect()
+	}
 }
