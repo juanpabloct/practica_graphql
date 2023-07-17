eef8fef9fc8544415d92a8bcc39d672b36ac95b8
juanpabloct <=>
2023-07-17 18:53:12 -0500

    Change types Resolver

--- src/rol/rol.resolver.ts
+++ src/rol/rol.resolver.ts
@@ -1,35 +1,36 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { RolService } from './rol.service';
-import { Rol } from './entities/rol.entity';
-import { CreateRolInput } from './dto/create-rol.input';
-import { UpdateRolInput } from './dto/update-rol.input';
+import { CreateRolInput } from './dto/create-rol.input'
+import { UpdateRolInput } from './dto/update-rol.input'
+import { RolObject } from './entities/rol.entity'
+import { RolService } from './rol.service'
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { RolCreateWithoutRolesAndPermisosInput } from 'src/@generated/prisma-nestjs-graphql/rol/rol-create-without-roles-and-permisos.input'
 
-@Resolver(() => Rol)
+@Resolver(() => RolObject)
 export class RolResolver {
-  constructor(private readonly rolService: RolService) {}
+	constructor(private readonly rolService: RolService) {}
 
-  @Mutation(() => Rol)
-  createRol(@Args('createRolInput') createRolInput: CreateRolInput) {
-    return this.rolService.create(createRolInput);
-  }
+	@Mutation(() => RolObject)
+	createRol(@Args('createRolInput') { name }: RolCreateWithoutRolesAndPermisosInput) {
+		return this.rolService.create(name)
+	}
 
-  @Query(() => [Rol], { name: 'rol' })
-  findAll() {
-    return this.rolService.findAll();
-  }
+	@Query(() => [RolObject], { name: 'rol' })
+	findAll() {
+		return this.rolService.findAll()
+	}
 
-  @Query(() => Rol, { name: 'rol' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.rolService.findOne(id);
-  }
+	@Query(() => RolObject, { name: 'rol' })
+	findOne(@Args('id', { type: () => Int }) id: number) {
+		return this.rolService.findOne(id)
+	}
 
-  @Mutation(() => Rol)
-  updateRol(@Args('updateRolInput') updateRolInput: UpdateRolInput) {
-    return this.rolService.update(updateRolInput.id, updateRolInput);
-  }
+	@Mutation(() => RolObject)
+	updateRol(@Args('updateRolInput') updateRolInput: UpdateRolInput) {
+		return this.rolService.update(updateRolInput.id, updateRolInput)
+	}
 
-  @Mutation(() => Rol)
-  removeRol(@Args('id', { type: () => Int }) id: number) {
-    return this.rolService.remove(id);
-  }
+	@Mutation(() => RolObject)
+	removeRol(@Args('id', { type: () => Int }) id: number) {
+		return this.rolService.remove(id)
+	}
 }
