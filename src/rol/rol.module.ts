662cb13fce7e718cf6231ca4fbea493461bc5923
juanpabloct <=>
2023-07-17 18:52:43 -0500

    Import prismaDbModule

--- src/rol/rol.module.ts
+++ src/rol/rol.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { RolService } from './rol.service';
-import { RolResolver } from './rol.resolver';
+import { RolResolver } from './rol.resolver'
+import { RolService } from './rol.service'
+import { Module } from '@nestjs/common'
+import { PrismaDbModule } from 'src/prisma-db/prisma-db.module'
 
 @Module({
-  providers: [RolResolver, RolService]
+	providers: [RolResolver, RolService],
+	imports: [PrismaDbModule],
 })
 export class RolModule {}
