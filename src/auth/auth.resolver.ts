104b1426d362ace6e584e770c0403c180b52a32f
juanpabloct <=>
2023-07-17 18:36:52 -0500

    Add mutation singOff

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,11 +1,13 @@
-import { AuthService } from './auth.service';
-import { SingInInput } from './inputs/sing-in.input';
-import { UsePipes } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { RolAnduser } from 'src/@generated/prisma-nestjs-graphql/rol-anduser/rol-anduser.model';
-import { CreateUserInput } from 'src/auth/inputs/create-user.input';
-import { EncryptPasswordPipe } from 'src/common/encript-password/encrypt-password.pipe';
-import { Login } from 'src/user/entities/user.entity';
+import { AuthService } from './auth.service'
+import { SingInInput } from './inputs/sing-in.input'
+import { SingOffInput } from './inputs/singOff.input'
+import { SingOffObject } from './interfaces/singOffObject'
+import { UsePipes } from '@nestjs/common'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { RolAnduser } from 'src/@generated/prisma-nestjs-graphql/rol-anduser/rol-anduser.model'
+import { CreateUserInput } from 'src/auth/inputs/create-user.input'
+import { EncryptPasswordPipe } from 'src/common/encript-password/encrypt-password.pipe'
+import { Login } from 'src/user/entities/user.entity'
 
 @Resolver()
 export class AuthResolver {
@@ -14,12 +16,17 @@ export class AuthResolver {
 	@UsePipes(EncryptPasswordPipe)
 	@Mutation(() => RolAnduser)
 	async singUp(@Args('SingUp') createUserInput: CreateUserInput) {
-		const newUser = await this.authService.SingUp(createUserInput);
-		return newUser;
+		const newUser = await this.authService.SingUp(createUserInput)
+		return newUser
 	}
 	@Mutation(() => Login)
 	async singIn(@Args('singIn') singIn: SingInInput) {
-		const session = await this.authService.SingIn(singIn);
-		return session;
+		const session = await this.authService.SingIn(singIn)
+		return session
+	}
+	@Mutation(() => SingOffObject)
+	async singOff(@Args('singOff') { id }: SingOffInput) {
+		const session = await this.authService.signOff(id)
+		return session
 	}
 }
