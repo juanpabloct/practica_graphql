3349ca54208346056f86a1c377252d392a727df6
juanpabloct <=>
2023-07-25 00:08:27 -0500

    change resolver permiso to PermisoMinAggregate

--- src/permisos/permisos.resolver.ts
+++ src/permisos/permisos.resolver.ts
@@ -1,3 +1,4 @@
+import { PermisoMinAggregate } from 'src/@generated/prisma-nestjs-graphql/permiso/permiso-min-aggregate.output'
 import { CreateManyPermisosInput } from './dto/create-many-permisos'
 import { UpdatePermisoInput } from './dto/update-permiso.input'
 import { PermisoObject } from './entities/permiso.entity'
@@ -6,36 +7,36 @@ import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { PermisoCreateWithoutRolesAndPermisosInput } from 'src/@generated/prisma-nestjs-graphql/permiso/permiso-create-without-roles-and-permisos.input'
 import { Permiso } from 'src/@generated/prisma-nestjs-graphql/permiso/permiso.model'
 
-@Resolver(() => PermisoObject)
+@Resolver(() => Permiso)
 export class PermisosResolver {
-	constructor(private readonly permisosService: PermisosService) {}
+	constructor(private readonly permisosService: PermisosService) { }
 
-	@Query(() => [Permiso], { name: 'permisos' })
+	@Query(() => [PermisoMinAggregate], { name: 'permisos' })
 	findAll() {
 		return this.permisosService.findAll()
 	}
 
-	@Query(() => Permiso, { name: 'permiso' })
+	@Query(() => PermisoMinAggregate, { name: 'permiso' })
 	findOne(@Args('id', { type: () => Int }) id: number) {
 		return this.permisosService.findOne(id)
 	}
 
-	@Mutation(() => Permiso)
+	@Mutation(() => PermisoMinAggregate)
 	createPermiso(@Args('createPermisoInput') createPermisoInput: PermisoCreateWithoutRolesAndPermisosInput) {
 		return this.permisosService.create(createPermisoInput)
 	}
-	@Mutation(() => [Permiso])
+	@Mutation(() => [PermisoMinAggregate])
 	async createPermisos(@Args('createPermisos') createPermisoInput: CreateManyPermisosInput) {
 		const permisos = await this.permisosService.createMany(createPermisoInput)
 		return permisos
 	}
 
-	@Mutation(() => Permiso)
+	@Mutation(() => PermisoMinAggregate)
 	async updatePermiso(@Args('updatePermisoInput') updatePermisoInput: UpdatePermisoInput) {
 		return this.permisosService.update(updatePermisoInput.id, updatePermisoInput)
 	}
 
-	@Mutation(() => PermisoObject)
+	@Mutation(() => PermisoMinAggregate)
 	removePermiso(@Args('id', { type: () => Int }) id: number) {
 		return this.permisosService.remove(id)
 	}
