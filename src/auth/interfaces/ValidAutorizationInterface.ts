dfef18fc6b9a7b8f48d338e396700100234a0f44
juanpabloct <=>
2023-07-25 00:11:56 -0500

    change name file import

--- src/auth/interfaces/ValidAutorizationInterface.ts
+++ src/auth/interfaces/ValidAutorizationInterface.ts
@@ -1,4 +1,4 @@
-import { Permisions } from '../validatePermisionsAndRoles/Enum.validatePermions'
+import { Permisions } from '../validatePermisionsAndRoles/Enum.validatePermitions'
 import { Roles } from '../validatePermisionsAndRoles/Enum.validateRol'
 
 export interface ValidAutorizationInterface {
