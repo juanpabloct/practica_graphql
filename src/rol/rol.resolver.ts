dac0190064a34066d467e8c65290f214ba28e207
juanpabloct <=>
2023-07-25 00:07:47 -0500

    change resolver RolObject to RolMinAggregate

--- src/rol/rol.resolver.ts
+++ src/rol/rol.resolver.ts
@@ -1,35 +1,35 @@
+import { RolMinAggregate } from 'src/@generated/prisma-nestjs-graphql/rol/rol-min-aggregate.output'
 import { CreateRolInput } from './dto/create-rol.input'
 import { UpdateRolInput } from './dto/update-rol.input'
-import { RolObject } from './entities/rol.entity'
 import { RolService } from './rol.service'
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { RolCreateWithoutRolesAndPermisosInput } from 'src/@generated/prisma-nestjs-graphql/rol/rol-create-without-roles-and-permisos.input'
 
-@Resolver(() => RolObject)
+@Resolver(() => RolMinAggregate)
 export class RolResolver {
-	constructor(private readonly rolService: RolService) {}
+	constructor(private readonly rolService: RolService) { }
 
-	@Mutation(() => RolObject)
+	@Mutation(() => RolMinAggregate)
 	createRol(@Args('createRolInput') { name }: RolCreateWithoutRolesAndPermisosInput) {
 		return this.rolService.create(name)
 	}
 
-	@Query(() => [RolObject], { name: 'rol' })
+	@Query(() => [RolMinAggregate], { name: 'roles' })
 	findAll() {
 		return this.rolService.findAll()
 	}
 
-	@Query(() => RolObject, { name: 'rol' })
+	@Query(() => RolMinAggregate, { name: 'rol' })
 	findOne(@Args('id', { type: () => Int }) id: number) {
 		return this.rolService.findOne(id)
 	}
 
-	@Mutation(() => RolObject)
+	@Mutation(() => RolMinAggregate)
 	updateRol(@Args('updateRolInput') updateRolInput: UpdateRolInput) {
 		return this.rolService.update(updateRolInput.id, updateRolInput)
 	}
 
-	@Mutation(() => RolObject)
+	@Mutation(() => RolMinAggregate)
 	removeRol(@Args('id', { type: () => Int }) id: number) {
 		return this.rolService.remove(id)
 	}
