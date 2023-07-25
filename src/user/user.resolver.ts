55f8b43559d11cd90e3d973674dc03dd069fdfc9
juanpabloct <=>
2023-07-25 00:05:38 -0500

    Change resolver User  to UserMinAggregate

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,3 +1,4 @@
+import { UserMinAggregate } from 'src/@generated/prisma-nestjs-graphql/user/user-min-aggregate.output'
 import { UpdateUserInput } from './dto/inputs/update-user.input'
 import { UserObject, UserWithoutPassword } from './entities/user.entity'
 import { UserService } from './user.service'
@@ -5,22 +6,27 @@ import { UseGuards, UsePipes } from '@nestjs/common'
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { User } from 'src/@generated/prisma-nestjs-graphql/user/user.model'
 import { ValidAutorization } from 'src/auth/customDecorator/validAutorization'
+import { RolesMeta } from 'src/auth/decorators/roles.decorator'
 import { JwtAuthGuard } from 'src/auth/guards/jwtGuard'
-import { Roles } from 'src/auth/validateRoles/validateRoles'
+import { RolesGuard } from 'src/auth/guards/roles.guard'
+import { Permisions } from 'src/auth/validatePermisionsAndRoles/Enum.validatePermions'
+import { Roles } from 'src/auth/validatePermisionsAndRoles/Enum.validateRol'
 import { EncryptPasswordPipe } from 'src/common/encript-password/encrypt-password.pipe'
 @Resolver(() => User)
 export class UserResolver {
-	constructor(private readonly userService: UserService) {}
-	@UseGuards(JwtAuthGuard)
+	constructor(private readonly userService: UserService) { }
+	//@ValidAutorization({ permiso: Permisions.READ_ALL, roles: [Roles.USER] })
+	//@UseGuards(JwtAuthGuard)
+	@RolesMeta([Roles.USER])
+	@UseGuards(RolesGuard)
 	@Query(() => [UserWithoutPassword], { name: 'users' })
 	async findAll(
-		@ValidAutorization([Roles.ADMIN]) User,
 	) {
 		const users = await this.userService.findAll()
 		return users
 	}
 
-	@Query(() => User, { name: 'user' })
+	@Query(() => UserMinAggregate, { name: 'user' })
 	async findOne(@Args('id', { type: () => Int }) id: number) {
 		return await this.userService.findOne(id)
 	}
