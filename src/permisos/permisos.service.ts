93b4a6507f7cd95d479524c700bf5d1479c6d1c1
juanpabloct <=>
2023-08-01 18:52:42 -0500

    Valid if exist permiso

--- src/permisos/permisos.service.ts
+++ src/permisos/permisos.service.ts
@@ -1,30 +1,39 @@
 import { CreateManyPermisosInput } from './dto/create-many-permisos'
 import { UpdatePermisoInput } from './dto/update-permiso.input'
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { PermisoCreateWithoutRolesAndPermisosInput } from 'src/@generated/prisma-nestjs-graphql/permiso/permiso-create-without-roles-and-permisos.input'
+import { PermisoMinAggregate } from 'src/@generated/prisma-nestjs-graphql/permiso/permiso-min-aggregate.output'
 import { Permisions } from 'src/@generated/prisma-nestjs-graphql/prisma/permisions.enum'
 import { PrismaService } from 'src/prisma-db/prisma-db.service'
 
 @Injectable()
 export class PermisosService {
-	constructor(private readonly prisma: PrismaService) {}
+	constructor(private readonly prisma: PrismaService) { }
 	entiti = this.prisma.permiso
-	async create(createPermisoInput: PermisoCreateWithoutRolesAndPermisosInput) {
-		return this.entiti.create({ data: { name: createPermisoInput.name } })
+	async create({ name }: PermisoCreateWithoutRolesAndPermisosInput) {
+		const existPermiso = await this.findOneName(name)
+		if (!existPermiso) {
+
+			return this.entiti.create({ data: { name } })
+		} else {
+			throw new BadRequestException(`Permiso ${name} already exist`)
+		}
 	}
 	async createMany(createPermisoInput: CreateManyPermisosInput) {
-		return this.entiti
+		await this.entiti
 			.createMany({
 				data: createPermisoInput.data.map((data) => ({
 					name: data,
 				})),
 				skipDuplicates: true,
 			})
-			.then(async () => {
-				return await createPermisoInput.data.map(async (values) => {
-					return await this.findOneName(values)
-				})
-			})
+		const data: PermisoMinAggregate[] = []
+		for (const permiso of createPermisoInput.data) {
+			const findPermiso = await this.findOneName(permiso);
+			data.push(findPermiso)
+		}
+
+		return data
 	}
 
 	async findAll() {
