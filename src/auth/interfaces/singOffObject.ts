4d307bd9807525a53c8b418b32c81d2c533f00b9
juanpabloct <=>
2023-08-01 18:51:44 -0500

    add fields User and rol

--- src/auth/interfaces/singOffObject.ts
+++ src/auth/interfaces/singOffObject.ts
@@ -1,5 +1,15 @@
-import { ObjectType, OmitType } from '@nestjs/graphql'
-import { User } from 'src/@generated/prisma-nestjs-graphql/user/user.model'
+import { Field, ObjectType, OmitType } from '@nestjs/graphql'
+import { RolAnduserMinAggregate } from 'src/@generated/prisma-nestjs-graphql/rol-anduser/rol-anduser-min-aggregate.output'
+import { RolMinAggregate } from 'src/@generated/prisma-nestjs-graphql/rol/rol-min-aggregate.output'
+import { UserMinAggregate } from 'src/@generated/prisma-nestjs-graphql/user/user-min-aggregate.output'
 
 @ObjectType()
-export class SingOffObject extends OmitType(User, ['password']) {}
+class SingInWithoutPassword extends OmitType(UserMinAggregate, ['password']) {
+}
+@ObjectType()
+export class SingOffObject extends RolAnduserMinAggregate {
+    @Field(() => RolMinAggregate, {nullable:true})
+    rol:RolMinAggregate
+    @Field(() => SingInWithoutPassword, {nullable:true})
+    User:SingInWithoutPassword
+}
